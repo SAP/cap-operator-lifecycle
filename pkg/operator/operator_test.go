1d74cd4ed8b72ba9f6bc2fa41ab9289a827c5963
i325261 <anirudh.prasad@sap.com>
2023-09-08 11:26:13 +0200

    Reset to basic tests

--- pkg/operator/operator_test.go
+++ pkg/operator/operator_test.go
@@ -8,16 +8,10 @@ import (
 	"flag"
 	"testing"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	fakediscovery "k8s.io/client-go/discovery/fake"
-	fakeclientset "k8s.io/client-go/kubernetes/fake"
 
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -50,72 +44,43 @@ func TestCheckDirectoryExists(t *testing.T) {
 	}
 }
 
-var _ = Describe("manger.Manager", func() {
-	Describe("New", func() {
-		It("should return an error if there is no Config", func() {
-			operator := New()
+func TestOperator(t *testing.T) {
+	{
+		operator := New()
 
-			utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-			utilruntime.Must(apiextensionsv1.AddToScheme(scheme))
-			utilruntime.Must(apiregistrationv1.AddToScheme(scheme))
+		utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+		utilruntime.Must(apiextensionsv1.AddToScheme(scheme))
+		utilruntime.Must(apiregistrationv1.AddToScheme(scheme))
 
-			operator.InitScheme(scheme)
+		operator.InitScheme(scheme)
 
-			opts := zap.Options{
-				Development: false,
-			}
-			opts.BindFlags(flag.CommandLine)
-			flag.Parse()
+		opts := zap.Options{
+			Development: false,
+		}
+		opts.BindFlags(flag.CommandLine)
+		flag.Parse()
 
-			ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+		ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
-			var chartDir string
-			var enableLeaderElection bool
+		var chartDir string
+		var enableLeaderElection bool
 
-			flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-				"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
-			flag.StringVar(&chartDir, "manifest-directory", "../../chart",
-				"The directory containing the deployment manifests for the managed operator.")
-			InitFlags(flag.CommandLine)
+		flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+			"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+		flag.StringVar(&chartDir, "manifest-directory", "../../chart",
+			"The directory containing the deployment manifests for the managed operator.")
+		InitFlags(flag.CommandLine)
 
-			if err := operator.ValidateFlags(); err != nil {
-				Expect(err).NotTo(HaveOccurred())
-				return
-			}
+		if err := operator.ValidateFlags(); err != nil {
+			t.Error("Flag validation failed")
+			return
+		}
 
-			// if name := operator.GetName(); name != "cap-operator.sme.sap.com" {
+		if name := operator.GetName(); name != "cap-operator.sme.sap.com" {
+			t.Error("Invalid name")
+			return
+		}
 
-			// }
-
-			mgr, err := ctrl.NewManager(cfg, ctrl.Options{
-				Scheme: scheme,
-				Client: client.Options{
-					Cache: &client.CacheOptions{
-						DisableFor: append(operator.GetUncacheableTypes(), &apiextensionsv1.CustomResourceDefinition{}, &apiregistrationv1.APIService{}),
-					},
-				},
-				LeaderElection:                enableLeaderElection,
-				LeaderElectionID:              operator.GetName(),
-				LeaderElectionReleaseOnCancel: true,
-			})
-
-			if err != nil {
-				Expect(err).NotTo(HaveOccurred())
-				return
-			}
-
-			fakeclientset := fakeclientset.NewSimpleClientset()
-			fakeDiscovery, ok := fakeclientset.Discovery().(*fakediscovery.FakeDiscovery)
-			if !ok {
-				//t.Error("fake discovery client creation failed")
-				Expect(ok).To(Equal(true))
-				return
-			}
-
-			if err := operator.Setup(mgr, fakeDiscovery); err != nil {
-				Expect(err).NotTo(HaveOccurred())
-				return
-			}
-		})
-	})
-})
+		// TODO - Add unit tests for setup
+	}
+}
