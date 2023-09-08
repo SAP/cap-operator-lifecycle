e900fd11facb75d98a1b6465a041b9e7e81a4450
anirudhprasad-sap <126493692+anirudhprasad-sap@users.noreply.github.com>
2023-09-08 15:17:34 +0200

    [MISC] Util package (#9)

---
+++ internal/util/util.go
@@ -0,0 +1,23 @@
+/*
+SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and cap-operator contributors
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package util
+
+import (
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+func CheckDirectoryExists(path string) error {
+	fsinfo, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	if !fsinfo.IsDir() {
+		return errors.Errorf("not a directory: %s", path)
+	}
+	return nil
+}
--- internal/util/util_test.go
+++ internal/util/util_test.go
@@ -2,28 +2,27 @@
 SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and cap-operator contributors
 SPDX-License-Identifier: Apache-2.0
 */
-package operator
 
-import (
-	"testing"
-)
+package util
+
+import "testing"
 
 func TestCheckDirectoryExists(t *testing.T) {
 
 	// invalid directory
-	if err := checkDirectoryExists("invalid"); err == nil {
+	if err := CheckDirectoryExists("invalid"); err == nil {
 		t.Error("error expected but not returned")
 		return
 	}
 
 	// valid directory
-	if err := checkDirectoryExists("../../chart"); err != nil {
+	if err := CheckDirectoryExists("../../chart"); err != nil {
 		t.Error("error not expected but returned")
 		return
 	}
 
 	// File path passed instead of a directory
-	if err := checkDirectoryExists("../../chart/values.yaml"); err == nil {
+	if err := CheckDirectoryExists("../../chart/values.yaml"); err == nil {
 		t.Error("error expected but not returned")
 		return
 	}
