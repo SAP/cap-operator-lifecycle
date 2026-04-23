546ec74e2f3bff44c9efe33379adf8feda88d940
Pavan <25031267+Pavan-SAP@users.noreply.github.com>
2026-04-23 11:09:13 +0200

    [Misc] Manager: minor cleanup (#198)
    - Move everything to 2026
    - remove commented code

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and cap-operator contributors
+SPDX-FileCopyrightText: 2026 SAP SE or an SAP affiliate company and cap-operator contributors
 SPDX-License-Identifier: Apache-2.0
 */
 
@@ -51,23 +51,13 @@ func init() {
 }
 
 func main() {
-	var metricsAddr string
-	var probeAddr string
-	// Uncomment the following lines to enable webhooks.
-	// var webhookAddr string
-	// var webhookCertDir string
+	var metricsAddr, probeAddr, chartDir string
 	var enableLeaderElection bool
-	var chartDir string
 
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080",
 		"The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081",
 		"The address the probe endpoint binds to.")
-	// Uncomment the following lines to enable webhooks.
-	// flag.StringVar(&webhookAddr, "webhook-bind-address", ":2443",
-	//	"The address the webhooks endpoint binds to.")
-	// flag.StringVar(&webhookCertDir, "webhook-tls-directory", "",
-	//	"The directory containing tls server key and certificate, as tls.key and tls.crt; defaults to $TMPDIR/k8s-webhook-server/serving-certs")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
 	flag.StringVar(&chartDir, "manifest-directory", "./chart",
@@ -90,13 +80,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Uncomment the following lines to enable webhooks.
-	// webhookHost, webhookPort, err := parseAddress(webhookAddr)
-	// if err != nil {
-	//	setupLog.Error(err, "error parsing webhook address")
-	//	os.Exit(1)
-	// }
-
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme: scheme,
 		Client: client.Options{
@@ -107,12 +90,6 @@ func main() {
 		LeaderElection:                enableLeaderElection,
 		LeaderElectionID:              operator.GetName(),
 		LeaderElectionReleaseOnCancel: true,
-		// Uncomment the following lines to enable webhooks.
-		// WebhookServer: webhook.NewServer(webhook.Options{
-		// 	Host:    webhookHost,
-		//	Port:    webhookPort,
-		//	CertDir: webhookCertDir,
-		// }),
 		Metrics: metricsserver.Options{
 			BindAddress: metricsAddr,
 		},
@@ -122,16 +99,6 @@ func main() {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
 	}
-
-	// Uncomment to enable conversion webhook (in case additional api versions are added in ./api).
-	// Note: to make conversion work, additional changes are necessary:
-	// - additional api versions have to be added to InitScheme() in pkg/operator/operator.go
-	// - one of the api versions has to marked as Hub, all other versions need to implement the
-	//   conversion.Convertible interface (see https://book.kubebuilder.io/multiversion-tutorial/conversion.html)
-	// - one of the api versions has to be marked as storage version (+kubebuilder:storageversion)
-	// - the crd resource has to be enhanced with a conversion section, telling the Kubernetes API server how to
-	//   connect to the conversion endpoint.
-	// mgr.GetWebhookServer().Register("/convert", conversion.NewWebhookHandler(mgr.GetScheme()))
 
 	if err := operator.Setup(mgr); err != nil {
 		setupLog.Error(err, "error registering controller with manager")
@@ -153,16 +120,3 @@ func main() {
 		os.Exit(1)
 	}
 }
-
-// Uncomment the following lines to enable webhooks.
-// func parseAddress(address string) (string, int, error) {
-//	host, p, err := net.SplitHostPort(address)
-//	if err != nil {
-//		return "", -1, err
-//	}
-//	port, err := strconv.Atoi(p)
-//	if err != nil {
-//		return "", -1, err
-//	}
-//	return host, port, nil
-// }
