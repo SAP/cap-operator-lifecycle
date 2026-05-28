56c4d3c4a186da9409710ec1e5d34fd41c2844f3
i325261 <anirudh.prasad@sap.com>
2026-05-28 14:39:27 +0200

    [Docs] Add doc.go with k8s:deepcopy-gen and groupName markers for API reference generation

---
+++ api/v1alpha1/doc.go
@@ -0,0 +1,10 @@
+/*
+SPDX-FileCopyrightText: 2026 SAP SE or an SAP affiliate company and cap-operator contributors
+SPDX-License-Identifier: Apache-2.0
+*/
+
+// +k8s:deepcopy-gen=package
+// +groupName=operator.sme.sap.com
+
+// Package v1alpha1 contains API Schema definitions for the operator v1alpha1 API group
+package v1alpha1
